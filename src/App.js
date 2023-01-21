86c25d4abb1eb0edce38f0b27d944e4d435b0afd
kiasy <dakiasy@gmail.com>
2023-01-20 21:16:16 -0800

    Initializing project with git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+//import Card from "./components/Card"
+//import Data from "./data"
 
-function App() {
+export default function App() {
+
+  const cards = Data.map(card => {
+    return <Card title={card.title}
+                        location={card.location}
+                        googleMapsUrl={card.googleMapsUrl}
+                        startDate={card.startDate}
+                        endDate={card.endDate}
+                        description={card.description}
+                        src={card.imageUrl}
+                         />
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {cards}
     </div>
-  );
+  ) 
 }
-
-export default App;
